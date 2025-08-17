55f362d8676d48ff05a0ad3bca7b5ec1e5f987f2
dandedotdev <contact@dande.dev>
2025-08-17 21:43:54 +0800

    feat(svelte-tutorial): Basic Svelte / Introduction / HTML tags

--- svelte/svelte-tutorial/src/lib/components/nested.svelte
+++
@@ -1,1 +0,0 @@
-<p>I am the son QQ.</p>
--- svelte/svelte-tutorial/src/routes/+page.svelte
+++ svelte/svelte-tutorial/src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import Nested from '$lib/components/nested.svelte';
-
 	let name = 'dandelion';
 </script>
+
+<svelte:head>
+  <meta name="description" content="This is a demo page for Svelte Tutorial" />
+	<title>Svelte Tutorial</title>
+</svelte:head>
 
 <div
 	class="flex h-dvh flex-col items-center justify-center gap-4 bg-amber-100 dark:bg-slate-900 dark:text-white"
@@ -13,5 +16,9 @@
 			{name}
 		</span>
 	</h1>
-	<Nested />
+	{@html `
+    <div class="p-4 w-88 h-68 bg-indigo-900 dark:bg-sky-900">
+      <img src="https://media.giphy.com/media/Ju7l5y9osyymQ/giphy-downsized.gif" alt="Rick roll" />
+    </div>
+  `}
 </div>
