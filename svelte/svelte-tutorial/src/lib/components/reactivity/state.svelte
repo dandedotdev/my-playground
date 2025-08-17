8d5628538a5f3a2eaddde7414e4bab6ccb685b10
dandedotdev <contact@dande.dev>
2025-08-17 22:09:37 +0800

    feat(svelte-tutorial): Basic Svelte / Reactivity / State

---
+++ svelte/svelte-tutorial/src/lib/components/reactivity/state.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let count = $state(0);
+
+  function handleClick() {
+    count++;
+  }
+</script>
+
+<button class="rounded-md bg-indigo-500 px-4 py-2 text-white dark:bg-sky-500" onclick={handleClick}>
+  Click me
+</button>
+<p>Count: {count}</p>
--- svelte/svelte-tutorial/src/routes/+page.svelte
+++ svelte/svelte-tutorial/src/routes/+page.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-	let name = 'dandelion';
+  import State from '$lib/components/reactivity/state.svelte';
+
+  let name = 'dandelion';
 </script>
 
 <svelte:head>
   <meta name="description" content="This is a demo page for Svelte Tutorial" />
-	<title>Svelte Tutorial</title>
+  <title>Svelte Tutorial</title>
 </svelte:head>
 
 <div
-	class="flex h-dvh flex-col items-center justify-center gap-4 bg-amber-100 dark:bg-slate-900 dark:text-white"
+  class="flex h-dvh flex-col items-center justify-center gap-4 bg-amber-100 dark:bg-slate-900 dark:text-white"
 >
-	<h1>
-		Hello
-		<span class="text-indigo-500 uppercase dark:text-sky-500">
-			{name}
-		</span>
-	</h1>
-	{@html `
-    <div class="p-4 w-88 h-68 bg-indigo-900 dark:bg-sky-900">
-      <img src="https://media.giphy.com/media/Ju7l5y9osyymQ/giphy-downsized.gif" alt="Rick roll" />
-    </div>
-  `}
+  <h1>
+    Hello
+    <span class="text-indigo-500 uppercase dark:text-sky-500">
+      {name}
+    </span>
+  </h1>
+  <State />
 </div>
