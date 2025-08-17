7c8da4031facfeb7cc38b92f8d8f173a0b9d11d6
dandedotdev <contact@dande.dev>
2025-08-17 18:57:44 +0800

    feat(svelte-tutorial): Basic Svelte / Introduction / Dynamic attributes

--- svelte/svelte-tutorial/src/routes/+page.svelte
+++ svelte/svelte-tutorial/src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-  let name = 'dandelion';
+	import src from '$lib/assets/HereAfter-logo-b.svg';
+
+	let name = 'dandelion';
+	let hereafterBrandName = 'HereAfter';
 </script>
 
-<h1 align="center">
-  Hello
-  <span class="text-indigo-500 dark:text-sky-500 uppercase">
-    {name}
-  </span>
-</h1>
+<div
+	class="flex h-dvh flex-col items-center justify-center gap-4 bg-amber-100 dark:bg-slate-900 dark:text-white"
+>
+	<h1>
+		Hello
+		<span class="text-indigo-500 uppercase dark:text-sky-500">
+			{name}
+		</span>
+	</h1>
+	<img {src} alt="{hereafterBrandName} logo (blue version)" />
+</div>
