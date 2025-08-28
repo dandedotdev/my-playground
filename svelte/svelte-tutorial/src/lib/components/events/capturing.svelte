8cbed3e794b0c171a2ab6c5402efcf0039b6d07b
dandedotdev <contact@dande.dev>
2025-08-28 21:27:50 +0800

    feat(svelte-tutorial): Basic Svelte / Events / Capturing

---
+++ svelte/svelte-tutorial/src/lib/components/events/capturing.svelte
@@ -0,0 +1,11 @@
+<div
+  class="rounded-md bg-sky-900"
+  onkeydowncapture={(e) => alert(`<div> ${e.key}`)}
+  role="presentation"
+>
+  <input
+    class="p-2"
+    placeholder="Type something"
+    onkeydowncapture={(e) => alert(`<input> ${e.key}`)}
+  />
+</div>
--- svelte/svelte-tutorial/src/routes/+page.svelte
+++ svelte/svelte-tutorial/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import InlineHandlers from '$lib/components/events/inline-handlers.svelte';
+  import Capturing from '$lib/components/events/capturing.svelte';
 
   const name = 'dandelion';
 </script>
@@ -18,5 +18,5 @@
       {name}
     </span>
   </h1>
-  <InlineHandlers />
+  <Capturing />
 </div>
